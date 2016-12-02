d80f159e1df5336e161391135d7caebddd38f519
Venelina Yanakieva <vyanakieva@melontech.com>
2016-12-02 13:54:05 +0200

    Beautify code
    - indentation, syntax, etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hangman v0.1';
   alphabet_chars = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'.split(',');
-  selected_letter = '';
   word = 'hippopotamus'.toUpperCase().split('');
+  selected_letter = '';
   already_tried = [];
   guessed_letters = [];
   lifes = 6;
@@ -18,55 +17,50 @@ export class AppComponent {
 
   onLetterSelected(e){
     this.selected_letter = e.target.innerText;
-    console.log(this.selected_letter);
     this.checkForMatch();
-    this.addToList(this.already_tried, this.selected_letter);
+    this._addToList(this.already_tried, this.selected_letter);
   }
   checkForMatch(){
-  	var letter = this.selected_letter;
-  	if (this.word.indexOf(letter) == -1
-  		&& this.already_tried.indexOf(letter) == -1){
-  		// no Match and letter is not tried
-	    this.hang();
-	} else {
-	    this.addToList(this.guessed_letters, this.selected_letter)
-	    this.checkSuccess()
-	    // Match found!
-	};
+    var letter = this.selected_letter;
+    if (!this._findInList(letter, this.word)
+      && this.already_tried.indexOf(letter) == -1){
+      this.hang(); // no Match and letter is not tried
+    } else { // Match found!
+      this._addToList(this.guessed_letters, letter)
+      this.checkSuccess()
+    };
   };
-  findInList(item, list){
-  	if (list.indexOf(item) > -1){
-  		return true;
-  	} else {
-  		return false;
-  	}
-  }
   checkSuccess(){
-  	var word_letters = new Set(this.word);
-  	// find the letters that remained not guessed
-  	var letters_not_guessed = Array.from(word_letters).filter((item) => {
-  		return !this.findInList(item, this.guessed_letters);
-  	})
-  	if (letters_not_guessed.length == 0){
-  		this.win = true;
-  	}
+    var word_letters = new Set(this.word);
+    // find the letters that remained not guessed
+    var letters_not_guessed = Array.from(word_letters).filter((item) => {
+        return !this._findInList(item, this.guessed_letters);
+    })
+    if (letters_not_guessed.length == 0){
+        this.win = true;
+    }
   };
   hang(){
-  	this.lifes--;
-  	if (this.lifes == 0){
-  		this.dead = true;
-  	};
+    this.lifes--;
+    if (this.lifes == 0){
+        this.dead = true;
+    };
   }
   ngOnInit(){
     this.guessed_letters.push(this.word[0])
     this.guessed_letters.push(this.word[this.word.length-1])
   }
 
-  addToList(list, item){
-  	// keep the list unique
-  	// this should be "utils" function one day
-  	if (list.indexOf(item) == -1){
-  		list.push(item)
-  	}
+  _findInList(item, list){
+    if (list.indexOf(item) > -1){
+        return true; // item is found in the list
+    } else {
+        return false; // item not found
+    }
+  }
+  _addToList(list, item){ // keep the list unique (this should be "utils" function one day)
+    if (list.indexOf(item) == -1){
+        list.push(item)
+    }
   }
 }
