2557433d32586a1dcdbde6ed4e183db151dc7415
Stela <stela.d.dineva@gmail.com>
2017-01-02 19:23:49 +0200

    Set loggedIn value on login user

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -6,26 +6,31 @@ import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 export class UserService {
   private loggedIn = false;
 
-  constructor(public angularfire: AngularFire) {}
+  constructor(public angularfire: AngularFire) {
+	this.loggedIn = !!localStorage.getItem('firebase:authUser:AIzaSyCH1lMo5k5sWXk4n4mYhLEdmBIEEOjrdpo:[DEFAULT]');
+  }
 
   login(email, password) {
-    return this.angularfire.auth.login(
-    	{
-	      email: email,
-	      password: password,
-	    },
-	    {
-	      provider: AuthProviders.Password,
-	      method: AuthMethods.Password,
-	    }
-    );
+	let result = this.angularfire.auth.login(
+		{
+		  email: email,
+		  password: password,
+		}, {
+		  provider: AuthProviders.Password,
+		  method: AuthMethods.Password,
+		}
+	).then((success) => {
+		this.loggedIn = true;
+	});
+	return result;
   }
   
   logout() {
-    return;
+	this.angularfire.auth.logout();
+	this.loggedIn = false;
   }
 
   isLoggedIn() {
-    return this.loggedIn;
+	return this.loggedIn;
   }
 }
